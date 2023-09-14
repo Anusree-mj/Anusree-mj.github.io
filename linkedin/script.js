653514190225ab6f6e24fe3aad01d1fd495520fe
Anusree <anu.mj0608@gmail.com>
2023-09-14 11:40:31 +0530

    add js for both input field

--- linkedin/script.js
+++ linkedin/script.js
@@ -1,9 +1,11 @@
-let password = document.getElementById("my-password").value;
-let email = document.getElementById("my-email").value;
 
+function hover(event){
+   event.stopPropagation();
+   console.log("fdgh");
+}
 
 function myFunction() {
-
+   let password = document.getElementById("my-password");
    if (password.type == "password") {
       document.getElementById("visibility").innerHTML = "Hide";
       password.type = "text";
@@ -15,16 +17,24 @@ function myFunction() {
 
 }
 function handleSignin() {
-   console.log("email")
-   if (email == "") {
-      document.getElementById("email_span").textContent = "Please enter an email address or phone number"    
+   let email = document.getElementById("my-email").value;
+   let password = document.getElementById("my-password").value;
+   let isError=false;
+   if (email === "") {
+      isError=true;
+      document.getElementById("email_span").textContent = "Please enter an email address or phone number"
    }
-   else{
-      document.getElementById("email_span").textContent=""
+   else {
+      document.getElementById("email_span").textContent = ""
    }
-   if (password == "") {
-      document.getElementById("password_span").textContent = "Please enter a password"    
-   }else{
-      document.getElementById("password_span").textContent =""
+   if (password === "") {
+      isError=true;
+      document.getElementById("password_span").textContent = "Please enter a password"
+   } else {
+      document.getElementById("password_span").textContent = ""
    }
+   if(!isError){
+      window.location.reload();
+   }
+  
 }
