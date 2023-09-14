6b312a82918c91cec20dd220bb73a9364d177ef7
Anusree <anu.mj0608@gmail.com>
2023-09-14 10:45:01 +0530

    fix js bug

--- linkedin/script.js
+++ linkedin/script.js
@@ -1,5 +1,5 @@
-let password = document.getElementById("my-password");
-let email = document.getElementById("my-email");
+let password = document.getElementById("my-password").value;
+let email = document.getElementById("my-email").value;
 
 
 function myFunction() {
@@ -13,4 +13,18 @@ function myFunction() {
       document.getElementById("visibility").innerHTML = "Show";
    }
 
+}
+function handleSignin() {
+   console.log("email")
+   if (email == "") {
+      document.getElementById("email_span").textContent = "Please enter an email address or phone number"    
+   }
+   else{
+      document.getElementById("email_span").textContent=""
+   }
+   if (password == "") {
+      document.getElementById("password_span").textContent = "Please enter a password"    
+   }else{
+      document.getElementById("password_span").textContent =""
+   }
 }
